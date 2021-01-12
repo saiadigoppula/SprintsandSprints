3778fc47a01fa0db997b30f71b6d935b8fa3f0e4
saiadigoppula <sai.adigoppula123@gmail.com>
2021-01-12 18:47:54 +0530

    javascript data insert - 1

--- Content/Programming/javascript/left_nav_items/left_nav_items.js
+++ Content/Programming/javascript/left_nav_items/left_nav_items.js
@@ -3,37 +3,37 @@ document.getElementById("myDIV").innerHTML +=`
   <a href="javascript_intro.html" class="button javascript_intro">JS Intro</a>  
   <a href="javascript_Comments.html" class="button javascript_Comments">JS Comments</a> 
   <a href="javascript_Syntax.html" class="button javascript_Syntax">JS Syntax</a>  
-  <a href="javascript_Variables.html" class="button avascript_Variables">JS Variables</a>
-  <a href="javascript_Operators.html" class="button javascript_intro">JS Operators</a> 
-  <a href="javascript_Data_Types.html" class="button javascript_intro">JS Data Types</a>
-  <a href="javascript_Strings.html" class="button javascript_intro">JS Strings</a> 
-  <a href="javascript_String_Methods.html" class="button javascript_intro">JS String Methods</a> 
-  <a href="javascript_Template_Literals.html" class="button javascript_intro">JS Template Literals</a> 
-  <a href="javascript_Numbers.html" class="button javascript_intro">JS Numbers</a>
-  <a href="javascript_Number_Methods.html" class="button javascript_intro">JS Number Methods</a>
-  <a href="javascript_Booleans.html" class="button javascript_intro">JS Booleans</a>
-  <a href="javascript_Type_Conversions.html" class="button javascript_intro">JS Type Conversions</a>
-  <a href="javascript_Conditions.html" class="button javascript_intro">JS Conditions</a> 
-  <a href="javascript_Switch.html" class="button javascript_intro">JS Switch</a> 
-  <a href="javascript_Arrays.html" class="button javascript_intro">JS Arrays</a>
-  <a href="javascript_Array_Methods.html" class="button javascript_intro">JS Array Methods</a>
-  <a href="javascript_Objects.html" class="button javascript_intro">JS Objects </a>
-  <a href="javascript_Date.html" class="button javascript_intro">JS Date</a>
-  <a href="javascript_Date_Methods.html" class="button javascript_intro">JS Date Methods</a>
-  <a href="javascript_Date_Formats.html" class="button javascript_intro">JS Date_Formats</a>
-  <a href="javascript_For_loop.html" class="button javascript_intro">JS For loop</a>
-  <a href="javascript_While_loop.html" class="button javascript_intro">JS While loop</a>
-  <a href="javascript_Break_continue.html" class="button javascript_intro">JS Break & continue</a>
-  <a href="javascript_Functions.html" class="button javascript_intro">JS Functions </a>
-  <a href="javascript_RegExp.html" class="button javascript_intro">JS RegExp</a>
-  <a href="javascript_Let.html" class="button javascript_intro">JS Let</a>
-  <a href="javascript_Const.html" class="button javascript_intro">JS Const</a>
-  <a href="javascript_Set_Datatype.html" class="button javascript_intro">JS Set Datatype</a>
-  <a href="javascript_Errors.html" class="button javascript_intro">JS Errors</a>
-  <a href="javascript_This_keyword.html" class="button javascript_intro">JS This keyword</a>
-  <a href="javascript_Arrow_Function.html" class="button javascript_intro">JS Arrow Function</a>
-  <a href="javascript_Arrow_Function_Methods.html" class="button javascript_intro">JS Arrow Function Methods</a>
-  <a href="javascript_Json.html" class="button javascript_intro">JS Json</a>
-  <a href="javascript_Classes.html" class="button javascript_intro">JS Classes</a>
+  <a href="javascript_Variables.html" class="button javascript_Variables">JS Variables</a>
+  <a href="javascript_Operators.html" class="button javascript_Operators">JS Operators</a> 
+  <a href="javascript_Data_Types.html" class="button javascript_Data_Types">JS Data Types</a>
+  <a href="javascript_Strings.html" class="button javascript_Strings">JS Strings</a> 
+  <a href="javascript_String_Methods.html" class="button javascript_String_Methods">JS String Methods</a> 
+  <a href="javascript_Template_Literals.html" class="button javascript_Template_Literals">JS Template Literals</a> 
+  <a href="javascript_Numbers.html" class="button javascript_Numbers">JS Numbers</a>
+  <a href="javascript_Number_Methods.html" class="button javascript_Number_Methods">JS Number Methods</a>
+  <a href="javascript_Booleans.html" class="button javascript_Booleans">JS Booleans</a>
+  <a href="javascript_Type_Conversions.html" class="button javascript_Type_Conversions">JS Type Conversions</a>
+  <a href="javascript_Conditions.html" class="button javascript_Conditions">JS Conditions</a> 
+  <a href="javascript_Switch.html" class="button javascript_Switch">JS Switch</a> 
+  <a href="javascript_Arrays.html" class="button javascript_Arrays">JS Arrays</a>
+  <a href="javascript_Array_Methods.html" class="button javascript_Array_Methods">JS Array Methods</a>
+  <a href="javascript_Objects.html" class="button javascript_Objects">JS Objects </a>
+  <a href="javascript_Date.html" class="button javascript_Date">JS Date</a>
+  <a href="javascript_Date_Methods.html" class="button javascript_Date_Methods">JS Date Methods</a>
+  <a href="javascript_Date_Formats.html" class="button javascript_Date_Formats">JS Date_Formats</a>
+  <a href="javascript_For_loop.html" class="button javascript_For_loop">JS For loop</a>
+  <a href="javascript_While_loop.html" class="button javascript_While_loop">JS While loop</a>
+  <a href="javascript_Break_continue.html" class="button javascript_Break_continue">JS Break & continue</a>
+  <a href="javascript_Functions.html" class="button javascript_Functions">JS Functions </a>
+  <a href="javascript_RegExp.html" class="button javascript_RegExp">JS RegExp</a>
+  <a href="javascript_Let.html" class="button javascript_Let">JS Let</a>
+  <a href="javascript_Const.html" class="button javascript_Const">JS Const</a>
+  <a href="javascript_Set_Datatype.html" class="button javascript_Set_Datatype">JS Set Datatype</a>
+  <a href="javascript_Errors.html" class="button javascript_Errors">JS Errors</a>
+  <a href="javascript_This_keyword.html" class="button javascript_This_keyword">JS This keyword</a>
+  <a href="javascript_Arrow_Function.html" class="button javascript_Arrow_Function">JS Arrow Function</a>
+  <a href="javascript_Arrow_Function_Methods.html" class="button javascript_Arrow_Function_Methods">JS Arrow Function Methods</a>
+  <a href="javascript_Json.html" class="button javascript_Json">JS Json</a>
+  <a href="javascript_Classes.html" class="button javascript_Classes">JS Classes</a>
 
 `;
